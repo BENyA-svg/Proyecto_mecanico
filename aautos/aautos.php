665ed3a6c5e9d8113f413b8063bd6d2fd6d238c2
AGUSTINA CARDOSO MARTINEZ <59835333@ceibal.edu.uy>
2025-10-24 18:22:47 -0300

    footer de contacto añadido

--- aautos/aautos.php
+++ aautos/aautos.php
@@ -211,13 +211,35 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
         </div>
     </div>
     <br>
+  <footer class="bg-dark text-white text-center py-3 mt-4">
+           <h2 class="h2">¡Vení a visitarnos!</h2>
+           <h1 class="h1">Y salí manejando tu auto como nuevo</h1><br><br>
+           <p class="contacto">Contactanos </p>
+           <div class="d-flex justify-content-between align-items-center flex-wrap px-3">
+             <div class="d-flex align-items-center">
+               <div class="logo-footer me-2"><img src="imagenes-inicio/sobre.png"></div>
+               <p class="logo-footer p">wolfcrewcontact@gmail.com</p>
+             </div>
+             <div class="d-flex align-items-center">
+               <div class="logo-footer me-2"><img src="imagenes-inicio/telefono.png"></div>
+               <p class="logo-footer p">+598 099 456 220</p>
+             </div>
+             <div class="d-flex align-items-center">
+               <div class="logo-footer me-2"><img src="imagenes-inicio/pinubicacion.png"></div>
+               <p class="logo-footer p">Con.José Pedro Varela 2737</p>
+             </div>
+           </div>
 
-      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+           <div class="map-container">
+<iframe src="https://www.google.com/maps/d/u/1/embed?mid=1_SIFaqqS37wGh6hIDiAiaXgrsSMJnGA&ehbc=2E312F" width="640" height="480"></iframe>
+          </div>
+          <p>&copy; 2024 JuancitoMotores. Todos los derechos reservados.</p>
+        </footer>
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-</body>
-</html>
-
-<?php
+    </body>
+    </html>
